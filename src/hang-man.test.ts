56e642951402e8cc0e0861dffa60c7f53669c5d2
German Delgado <germandg6@gmail.com>
2025-05-20 12:21:38 +0100

    red: hangman  should receive a secret word that contains only letters

--- src/hang-man.test.ts
+++ src/hang-man.test.ts
@@ -1,3 +1,11 @@
+import { Hangman } from './hang-man';
 describe('The Hangman ', () => {
-  it('', () => {});
+  it('should receive a secret word that contains only letters', () => {
+    const secretWord = 'house!';
+    const tries = 3;
+
+    const game = Hangman.startGame(secretWord, tries);
+
+    expect(game.error()).toBe(HangmanError.InvalidSecretWord);
+  });
 });
