a338f99bfc20170e692bc462c5372153402c1911
German Delgado <germandg6@gmail.com>
2025-05-20 12:32:20 +0100

    green: should receive a positive number of tries

--- src/hangman.ts
+++ src/hangman.ts
@@ -3,9 +3,14 @@ export class Hangman {
     const hasInvalidCharacters = /[^a-zA-Z]+$/.test(secretWord);
 
     if (hasInvalidCharacters) return HangmanError.InvalidSecretWord;
+
+    const hasNegativeOrZeroTries = tries <= 0;
+
+    if (hasNegativeOrZeroTries) return HangmanError.TriesMustBeAtLeastOne;
   }
 }
 
 export enum HangmanError {
   InvalidSecretWord,
+  TriesMustBeAtLeastOne,
 }
