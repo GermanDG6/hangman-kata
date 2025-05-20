4092b6d68d9ea75b820a3e776ac720bd5bdea2a3
German Delgado <germandg6@gmail.com>
2025-05-20 12:45:57 +0100

    red: hangman does not allow multiple letters at each trial

--- src/hangman.test.ts
+++ src/hangman.test.ts
@@ -1,20 +1,28 @@
-import { Hangman, HangmanError } from './hangman';
+import { GameError, Hangman } from './hangman';
 describe('The Hangman ', () => {
   it('should receive a secret word that contains only letters', () => {
     const secretWord = 'house!';
-    const tries = 3;
+    const trials = 3;
 
-    const game = Hangman.startGame(secretWord, tries);
+    const game = Hangman.startGame(secretWord, trials);
 
-    expect(game).toBe(HangmanError.InvalidSecretWord);
+    expect(game).toBe(GameError.InvalidSecretWord);
   });
 
-  it('should receive a positive number of tries', () => {
+  it('should receive a positive number of trials', () => {
     const secretWord = 'house';
-    const tries = -3;
+    const trials = -3;
 
-    const game = Hangman.startGame(secretWord, tries);
+    const game = Hangman.startGame(secretWord, trials);
 
-    expect(game).toBe(HangmanError.TriesMustBeAtLeastOne);
+    expect(game).toBe(GameError.TrialsMustBeAtLeastOne);
+  });
+
+  it('does not allow multiple letters at each trial', () => {
+    const game = Hangman.startGame('house', 4);
+
+    const trial = game.tryTo('ab');
+
+    expect(trial).toBe(GameError.MultipleLettersNotAllowed);
   });
 });
